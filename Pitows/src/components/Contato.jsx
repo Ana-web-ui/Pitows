98d56d8d4e16a985289f2e559b6ee2e7e7e14f1c
ana leite <anacarolinaleite1401@gmail.com>
2024-12-02 20:09:44 -0300

    contato pronto

--- Pitows/src/components/Contato.jsx
+++ Pitows/src/components/Contato.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-const Contato = () =>{
-    return <div className="container-contact">
-        <h2>Peças Sob Medida</h2>
-        <p>Visite nossa alfaiataria e encomende sua peça sob medida. Tenha um modelo feito especialmente para você.</p>
+const Contato = () => {
+  return (
+    <>
+      <div className="section-contact">
+        <div className="container-contact">
+          <h2>Peças Sob Medida</h2>
+          <p>
+            Visite nossa alfaiataria e encomende sua peça sob medida. Tenha um
+            modelo feito especialmente para você.
+          </p>
 
-        <button type="submit">Entre em contato via whatsapp</button>
-    </div>
-}
+          <button type="submit">Entre em contato via whatsapp</button>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Contato;
